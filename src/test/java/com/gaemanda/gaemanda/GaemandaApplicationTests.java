d0e4194d4995a65e567a8def2b461fed83983711
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-02-17 22:12:50 +0900

    first commit

---
+++ src/test/java/com/gaemanda/gaemanda/GaemandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaemanda.gaemanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaemandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
